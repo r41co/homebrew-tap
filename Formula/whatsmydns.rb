74959a53a9909e91f9f64ae920fb20d33c433432
T. Tran <t-tran@users.noreply.github.com>
2020-09-20 16:03:02 +0700

    Update whatsmydns

--- Formula/whatsmydns.rb
+++ Formula/whatsmydns.rb
@@ -3,8 +3,8 @@ class Whatsmydns < Formula
   homepage "https://r41.co"
 
   url "https://raw.githubusercontent.com/t-tran/shell/master/whatsmydns"
-  version "1.0.0"
-  sha256 "24d7ee7b36a6009d6f1c79f240353820b958da1e670536a6b4ce22831dd0b4dc"
+  version "1.0.1"
+  sha256 "afbda0cf9c87d47d1777475c9091dad5f8f1f4f62c28a0a40a6ebf275be71f55"
   bottle :unneeded
 
   conflicts_with "whatsmydns"
